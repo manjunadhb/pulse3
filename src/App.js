e89d51ee79f238db3ea31a3f6bd9c48f5e89f36c
manjunadhb <manjunadhb@gmail.com>
2022-04-08 13:15:43 +0530

    removed unwanted code

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>RRR is the main grosser of Movies at Present</h1>
+      <h1>The Kashmir Files is the second main grosser of movies at present</h1>
     </div>
   );
 }
