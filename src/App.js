2f54c91e751234dca020cc6b914176b5d1eb6314
manjunadhb <manjunadhb@gmail.com>
2022-04-08 13:17:13 +0530

    add KGF

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>RRR is the main grosser of Movies at Present</h1>
       <h1>The Kashmir Files is the second main grosser of movies at present</h1>
+      <h1>Next Big movie we are expecting is KGF</h1>
     </div>
   );
 }
